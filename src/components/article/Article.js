0d686ce7f77d490b2ca170295d8964617c2e6c5e
Marco Polo Bustillo <marco.polo@gmail.com>
2020-04-09 11:18:20 +0800

    Add skeleton loading on feed

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Typography, Grid } from "@material-ui/core";
 import ArticleItem from "./ArticleItem";
+import ArticleLoading from "./ArticleLoading";
 import { getData } from "../../utils/api";
 
 const Article = (props) => {
   const [state, setState] = useState({
     loading: false,
     articles: [],
+    articleLoading: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   });
 
   const getFeed = async () => {
+    setState({ ...state, loading: true });
     const result = await getData("/feed");
     if (result) {
       setState({ ...state, loading: false, articles: result.data });
@@ -24,6 +27,12 @@ const Article = (props) => {
     <>
       <Typography variant="h4">Articles</Typography>
       <Grid container spacing={3}>
+        {state.loading &&
+          state.articleLoading.map((article) => (
+            <Grid key={article} item xs={12} md={6} lg={4}>
+              <ArticleLoading />
+            </Grid>
+          ))}
         {state.articles.length > 0 &&
           state.articles.map((article) => (
             <Grid key={article.id} item xs={12} md={6} lg={4}>
---
+++ src/components/article/ArticleLoading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Skeleton from "@material-ui/lab/Skeleton";
+
+const ArticleLoading = () => {
+  return <Skeleton variant="rect" height={150} />;
+};
+
+export default ArticleLoading;
