8de5f30b992132e71cf02f65f4708a808ad49e11
Marco Polo Bustillo <marco.polo@gmail.com>
2020-04-09 18:12:48 +0800

    Change skeleton height to 200

--- src/components/article/ArticleLoading.js
+++ src/components/article/ArticleLoading.js
@@ -2,7 +2,7 @@ import React from "react";
 import Skeleton from "@material-ui/lab/Skeleton";
 
 const ArticleLoading = () => {
-  return <Skeleton variant="rect" height={150} />;
+  return <Skeleton variant="rect" height={200} />;
 };
 
 export default ArticleLoading;
